ddb52ef6fd4b8f08fae8b777a5969cd919dfde9e
pnowak86 <p_nowak86@poczta.fm>
2018-03-16 13:23:20 +0100

    second commit

--- src/navigateOnSystem2/Main.java
+++ src/navigateOnSystem2/Main.java
@@ -5,14 +5,38 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	Scanner scanner = new Scanner(System.in);
-	boolean running = false;
+        Scanner scanner = new Scanner(System.in);
+        boolean running = false;
 
-	while(!running){
-        System.out.println("$>");
-        String userInput = scanner.nextLine();
-        System.out.println("User said " + userInput);
-    }
+        while (!running) {
+            System.out.println("$>");
+            String userInput = scanner.nextLine();
+            System.out.println("User said " + userInput);
+
+            switch (userInput) {
+
+                case "dir":
+                    //implement method
+                    break;
+                case "cd":
+                    //change directory
+                    break;
+                case "cd..":
+                    //go up in directories
+                    break;
+                case "prompt":
+                    //while loop
+                    //implement customized promt
+
+                    
+                    userInput = scanner.nextLine();
+
+
+                    break;
+
+
+            }
+        }
 
 
     }
